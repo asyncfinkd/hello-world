84ba2ca6ec72086246950e06f46b25503e16c5c7
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-04-29 19:34:24 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,16 +1,5 @@
-package com.example.helloworld;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class HelloWorld extends Activity {
-   /** Called when the activity is first created. */
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       TextView tv = new TextView(this);
-       tv.setText("Hello World");
-       setContentView(tv);
-   }
+public class main {
+  public static void main(String[], args) {
+    System.out.println("Hello World");
+  }
 }
